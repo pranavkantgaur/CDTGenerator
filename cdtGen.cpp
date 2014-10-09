86c996d27086e3519f74c29a031936ce8dd1e633
Pranav <pranav8iisc@gmail.com>
2014-10-09 17:32:57 +0530

    Initial commit

---
+++ cdtGen.cpp
@@ -0,0 +1,46 @@
+#include <CGAL/Delaunay_triangulation_3.h>
+
+using namespace std;
+
+/// Datastructure implementations:
+// PLC: Implement using CGAL Linear cell complexes
+// Delaunay tetrahedralization: CGAL::Delaunay_3
+// Output mesh: Linear cell complex
+
+
+void readPLCInput()
+{
+    // read PLY file(assumed to contain the PLC)
+    // initialize inputVertices
+    // initialize inputFaces
+
+
+    return;
+}
+
+
+void computeInitialDelaunayTetrahedralization()
+{
+
+    return;
+}
+
+void removeLocalDegeneracies()
+{
+return;
+}
+
+void recoverConstraintFaces()
+{
+return;
+}
+
+
+int main()
+{
+    readPLCInput();
+    computeInitialDelaunayTetrahedralization();
+    removeLocalDegeneracies();
+    recoverConstraintFaces();
+    return 0;
+}
