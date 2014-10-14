284c69dbd6d884c2f827b0d0b4f434c06525ad04
Pranav <pranav8iisc@gmail.com>
2014-10-14 12:24:53 +0530

    included plc and delaunay classes

--- cdtGen.cpp
+++ cdtGen.cpp
@@ -1,13 +1,10 @@
-#include <CGAL/Delaunay_triangulation_3.h>
-
-using namespace std;
-
-// Datastructure implementations:
-// PLC: Implement using CGAL Linear cell complexes
-// Delaunay tetrahedralization: CGAL::Delaunay_3
-// Output mesh: Linear cell complex
+#include <piecewiseLinearComplex.h>
+#include <delaunayTriangulation.h>
 
 
+
+
+// reads input PLC
 void readPLCInput()
 {
 	// read PLY file(assumed to contain the PLC)
@@ -18,35 +15,41 @@ void readPLCInput()
 	
 }
 
+// computes the initial delaunay tetrahedralization
 void computeInitialDelaunayTetrahedralization()
 {
 
     	
 }
 
+// removes local degeneracies from Delaunay tetrahedralization
 void removeLocalDegeneracies()
 {
 	// compute all local degeneracies in DT and add them to Q
-	// for each local degeneracy l in Q:
-		// if l is removable:
-			// remove l by small perturbation
-		// else,
-			// compute vb to break l
-				// if vb encroaches upon any segment or subface,
-					// push l into queue
-					// call boundary protection procedure
-				// else
-					// insert vb to break l 	
-				// end if
-		// end if
-	// end for
+	// repeat while Q != NULL
+		// for each local degeneracy l in Q:
+			// if l is removable:
+				// remove l by small perturbation
+			// else,
+				// compute vb to break l
+					// if vb encroaches upon any segment or subface,
+						// push l into queue
+						// call boundary protection procedure
+					// else
+						// insert vb to break l 	
+					// end if
+			// end if
+		// end for
+	// end repeat
 }	
 
+// recovers the constraint faces
 void recoverConstraintFaces()
 {
 	
 }
 
+// main procedure
 int main()
 {
 	readPLCInput();
