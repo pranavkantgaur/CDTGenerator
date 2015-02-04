c46bcde84072a384da4f53be4c6021ec0c87c2d6
Pranav <pranav8iisc@gmail.com>
2015-02-04 17:16:14 +0530

    fixed compilation warning

--- cdtGen.cpp
+++ cdtGen.cpp
@@ -211,7 +211,7 @@ void writePLYOutput()
 	// use rPLY for writing DT to PLY file
 	p_ply delaunayMeshPLY;
 
-	if((delaunayMeshPLY = ply_create("delaunay.ply", PLY_ASCII, NULL, NULL, NULL)) == NULL)
+	if((delaunayMeshPLY = ply_create("delaunay.ply", PLY_ASCII, NULL, 0, NULL)) == NULL)
 	{
 		cout << "\nCannot write Mesh output!!";
 		exit(0);
@@ -220,28 +220,31 @@ void writePLYOutput()
 	else
 	{
 		ply_add_element(delaunayMeshPLY, "vertex", DT.number_of_vertices()); 
-	      	ply_add_property(delaunayMeshPLY, "x", 	PLY_FLOAT, PLY_UIN32, PLY_FLOAT);
-		ply_add_property(delaunayMeshPLY, "y", PLY_FLOAT, PLY_UIN32, PLY_FLOAT);
-		ply_add_property(delaunayMeshPLY, "z", PLY_FLOAT, PLY_UIN32, PLY_FLOAT);
+	      	ply_add_scalar_property(delaunayMeshPLY, "x", PLY_FLOAT);
+		ply_add_scalar_property(delaunayMeshPLY, "y", PLY_FLOAT);
+		ply_add_scalar_property(delaunayMeshPLY, "z", PLY_FLOAT);
 		
+		ply_add_element(delaunayMeshPLY, "face", DT.number_of_facets());
+		ply_add_list_property(delaunayMeshPLY, "vertex_indices", PLY_UCHAR, PLY_INT32);	
+
+	
 		float x, y, z;
 		Point p;
-
+	
 		for (Delaunay::Finite_vertices_iterator vIter = DT.finite_vertices_begin(); vIter != DT.finite_vertices_end(); vIter++)
 		{
 			p = vIter->point();
-			x = p.x();
+			 
+			x = p.x();			
 			y = p.y();
 			z = p.z();
-
+			
+		
 			ply_write(delaunayMeshPLY, x);
 			ply_write(delaunayMeshPLY, y);
 			ply_write(delaunayMeshPLY, z);
 		}
 		
-		ply_add_element(delaunayMeshPLY, "face", DT.number_of_facets());
-		ply_add_list_property(delaunayMeshPLY, "vertex_indices", PLY_UCHAR, PLY_INT32);	
-
 		for (Delaunay::Finite_facets_iterator fIter = DT.finite_facets_begin(); fIter != DT.finite_facets_end(); fIter++)
 		{
 			ply_write(delaunayMeshPLY, 3);
@@ -250,7 +253,9 @@ void writePLYOutput()
 			ply_write(delaunayMeshPLY, (fIter->first)->vertex(2)->info());
 		}
 
-		ply_write_header(delaunayMeshPLY);
+		if(!ply_write_header(delaunayMeshPLY))
+			cout << "\nHeader not writen" << flush;
+
 		ply_close(delaunayMeshPLY);
 	}
 }
